65bc863160430abe4f4a730491fe09003217f3c9
Jimin Park <pjm950@gmail.com>
2025-09-29 19:23:08 +0900

    Added skeleton code for file I/O.

--- src/message/index.ts
+++ src/message/index.ts
@@ -2,3 +2,4 @@ export type { Message, MessageArrayLike, ContentPart } from "./schema.js";
 export { validateMessage, validateMessageArray, isMessageArray, asMessageArray } from "./schema.js";
 
 export * from "./codec.js";
+export * from "./file.js";
