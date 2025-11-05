8edf7c0258773e03a5bd842d65c62ebbcb07c1a2
Jimin Park <pjm950@gmail.com>
2025-11-05 13:11:52 +0900

    Organized decoder tests.

--- src/file-codec/stf/decoder.spec.ts
+++ src/file-codec/stf/decoder.spec.ts
@@ -7,12 +7,6 @@ const decoder = createDecoder(createSTFDecoder);
 
 describe("file-codec/stf", () => {
     describe("STFCodec.createDecoder", () => {
-        it("should decode an empty STF file as an empty array", () => {
-            for(const serialized of ["", " ", "\t", "\n", " \n "]) {
-                assert.deepStrictEqual(decoder(serialized), { messages: []});
-            }
-        });
-
         it("should decode a simple STF file", () => {
             const serialized = [
                 ";user",
@@ -28,47 +22,169 @@ describe("file-codec/stf", () => {
             ] satisfies Message[]);
         });
 
-        it("should decode messages with attributes and escaped data lines", () => {
-            const serialized = [
-                "",
-                ";msg role=user name=\"Jane Doe\" id='123'",
-                "Hello,",
-                ";;Important notice",
-                "",
-            ].join('\n');
+        context("line handling", () => {
+            it("should decode an empty STF file as an empty array", () => {
+                for(const serialized of ["", " ", "\t", "\n", " \n "]) {
+                    assert.deepStrictEqual(decoder(serialized), { messages: []});
+                }
+            });
 
-            const { messages } = decoder(serialized);
-            assert.deepStrictEqual(messages, [
-                {
-                    role: 'user',
-                    name: 'Jane Doe',
-                    id: '123',
-                    content: "Hello,\n;Important notice\n",
-                },
-            ] satisfies Message[]);
+            it("should treat lines with leading blanks before ';' as data lines", () => {
+                const serialized = [
+                    ";user",
+                    " ; this is not a command",
+                ].join('\n');
+
+                const { messages } = decoder(serialized);
+                assert.deepStrictEqual(messages, [
+                    {role: 'user', content: " ; this is not a command"},
+                ] satisfies Message[]);
+            });
+
+            it("should handle escaped data lines", () => {
+                const serialized = [
+                    ";user",
+                    ";;this is not a command",
+                ].join('\n');
+
+                const { messages } = decoder(serialized);
+                assert.deepStrictEqual(messages, [
+                    {role: 'user', content: ";this is not a command"},
+                ] satisfies Message[]);
+            });
+
+            it("should ignore line and block comments", () => {
+                const serialized = [
+                    ";# this is a comment",
+                    ";/* block comment start",
+                    ";developer",
+                    ";*/",
+                    ";user",
+                    "Hello",
+                    ";assistant",
+                    "; // comment ignored",
+                    "Hi",
+                    ";developer",
+                    "Howdy",
+                ].join('\n');
+
+                const { messages } = decoder(serialized);
+                assert.deepStrictEqual(messages, [
+                    {role: 'user', content: "Hello"},
+                    {role: 'assistant', content: "Hi"},
+                    {role: 'developer', content: "Howdy"},
+                ] satisfies Message[]);
+            });
         });
 
-        it("should ignore line and block comments", () => {
-            const serialized = [
-                ";# this is a comment",
-                ";/* block comment start",
-                ";developer",
-                ";*/",
-                ";user",
-                "Hello",
-                ";assistant",
-                "; // comment ignored",
-                "Hi",
-                ";developer",
-                "Howdy",
-            ].join('\n');
+        context("message arguments", () => {
+            it("should handle key-value arguments for commands", () => {
+                const serialized = ";msg role=user name=\"John Doe\" id=123";
+                const { messages } = decoder(serialized);
+                assert.deepStrictEqual(messages, [
+                    {
+                        role: 'user',
+                        name: 'John Doe',
+                        id: '123',
+                        content: "",
+                    },
+                ] satisfies Message[]);
+            });
 
-            const { messages } = decoder(serialized);
-            assert.deepStrictEqual(messages, [
-                {role: 'user', content: "Hello"},
-                {role: 'assistant', content: "Hi"},
-                {role: 'developer', content: "Howdy"},
-            ] satisfies Message[]);
+            it("should handle JSON5 arguments for commands", () => {
+                const serialized = ";msg {role: 'user', name: 'John Doe', id: '456'}";
+                const { messages } = decoder(serialized);
+                assert.deepStrictEqual(messages, [
+                    {
+                        role: 'user',
+                        name: 'John Doe',
+                        id: '456',
+                        content: "",
+                    },
+                ] satisfies Message[]);
+            });
+        });
+        
+        context("message commands", () => {
+            it("should decode role-specific commands and role inheritance", () => {
+                const serialized = [
+                    ";system",
+                    "You are a helpful assistant.",
+                    ";user",
+                    "Hello!",
+                    ";msg",
+                    "Hello again!",
+                    ";dev",
+                    "Developer message.",
+                    ";tool",
+                    "Tool message.",
+                ].join('\n');
+
+                const { messages } = decoder(serialized);
+                assert.deepStrictEqual(messages, [
+                    {role: 'system', content: "You are a helpful assistant."},
+                    {role: 'user', content: "Hello!"},
+                    {role: 'user', content: "Hello again!"},
+                    {role: 'developer', content: "Developer message."},
+                    {role: 'tool', content: "Tool message."},
+                ] satisfies Message[]);
+            });
+
+            it("should decode a 'raw' message", () => {
+                const serialized = [
+                    ";raw",
+                    "{",
+                    "  role: 'user',",
+                    "  content: 'raw content',",
+                    "}",
+                    ";end",
+                ].join('\n');
+                const { messages } = decoder(serialized);
+                assert.deepStrictEqual(messages, [
+                    {
+                        role: 'user',
+                        content: "raw content",
+                    },
+                ] satisfies Message[]);
+            });
+        });
+
+        context("misc commands", () => {
+            it("should handle the 'flush' command", () => {
+                const serialized = [
+                    ";user",
+                    "Hello!",
+                    ";flush",
+                    "   ",
+                    ";assistant",
+                    "Hi!",
+                ].join('\n');
+                const { messages } = decoder(serialized);
+                assert.deepStrictEqual(messages, [
+                    {role: 'user', content: 'Hello!'},
+                    {role: 'assistant', content: 'Hi!'},
+                ]);
+            });
+        })
+
+        context("error handling", () => {
+            it("should throw an error for data lines without a message", () => {
+                const serialized = "some content";
+                assert.throws(() => decoder(serialized));
+            });
+    
+            it("should throw an error for unmatched block comments", () => {
+                assert.throws(() => decoder(";/* this is an unterminated block comment"));
+                assert.throws(() => decoder(";*/ this is a closing block comment without an opening one"));
+            });
+
+            it("should throw an error for using role inheritance without a previous message", () => {
+                assert.throws(() => decoder(";msg"));
+            });
+
+            it("should throw an error for an unexpected 'end' command", () => {
+                assert.throws(() => decoder(";end"));
+            });
         });
     });
 });
