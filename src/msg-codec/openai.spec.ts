53de3752a82086948abf70547dd5606d972ba13b
Jimin Park <pjm950@gmail.com>
2025-11-09 21:51:50 +0900

    Added tests for OpenAIChatOutputCodec.

---
+++ src/msg-codec/openai.spec.ts
@@ -0,0 +1,16 @@
+import { assert } from 'chai';
+import { ChatCompletionMessage } from 'openai/resources';
+
+import { OpenAIChatOutputCodec } from "./openai.js";
+
+describe("OpenAIChatOutputCodec", () =>{
+    it("should be able to decode simple messages", () => {
+        const api_messages: ChatCompletionMessage[] = [
+            {role: 'assistant', content: "", refusal: null},
+            {role: 'assistant', content: "Hello, world!", refusal: null},
+        ];
+
+        const messages = OpenAIChatOutputCodec.createDecoder()(api_messages);
+        assert.strictEqual(messages.length, api_messages.length);
+    });
+});
