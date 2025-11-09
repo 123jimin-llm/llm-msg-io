87b482afd618b3871332fb234a8d46a7c89189d6
Jimin Park <pjm950@gmail.com>
2025-11-09 22:13:16 +0900

    Fixed incorrect errors.

--- src/message/schema/message.spec.ts
+++ src/message/schema/message.spec.ts
@@ -1,6 +1,6 @@
 import { assert } from 'chai';
 
-import { Message } from "./message.js";
+import { Message, MessageArray } from "./message.js";
 
 describe("Message", () =>{
     it("should assert valid messages", () => {
@@ -14,16 +14,27 @@ describe("Message", () =>{
     });
 
     it("should allow optional fields set to undefined", () => {
-        const msg: Record<string, string> = {role: 'user', content: "Hello!"};
+        const msg: Record<string, string> = {role: 'assistant', content: "Hello!"};
 
         for(const field of ['id', 'name', 'reasoning', 'tool_calls']) {
             const msg_with_undefined: unknown = {...msg, [field]: (void 0)};
-            assert.doesNotThrow(() => Message.assert(msg_with_undefined), `Field: ${field}`);
+            assert.doesNotThrow(() => Message.assert(msg_with_undefined));
         }
     });
 
     it("should allow custom roles", () => {
         const msg = {role: 'custom_role', content: "Hello!"};
         assert.doesNotThrow(() => Message.assert(msg));
+    });
+});
+
+describe("MessageArray", () => {
+    it("should allow optional fields set to undefined", () => {
+        const msg: Record<string, string> = {role: 'assistant', content: "Hello!"};
+
+        for(const field of ['id', 'name', 'reasoning', 'tool_calls']) {
+            const msg_with_undefined: unknown = {...msg, [field]: (void 0)};
+            assert.doesNotThrow(() => MessageArray.assert([msg_with_undefined]));
+        }
     });
 });
