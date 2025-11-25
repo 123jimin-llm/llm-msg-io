18f76e74433c41062ee0301b1062157185b87db3
Jimin Park <pjm950@gmail.com>
2025-11-25 19:11:25 +0900

    Added `refusal` and `extra`. Added comments.

--- src/message/schema/message.ts
+++ src/message/schema/message.ts
@@ -7,18 +7,35 @@ export const ToolCall = exportType(type({
     "id?": "string",    
     name: "string",
     arguments: "string",
+
+    "extra?": "unknown",
 }));
 export type ToolCall = typeof ToolCall.infer;
 
 export interface Message {
+    /** Unique identifier for a message. */
     id?: string|undefined;
-    name?: string|undefined;
+
+    /** Role of the sender. */
     role: string;
 
+    /** Name of the sender. */
+    name?: string|undefined;
+
+    /** Main content of the message (corresponds to `final` channel). */
     content: MessageContent;
+
+    /** Reasoning data (corresponds to `analysis` channel). */
     reasoning?: MessageContent|undefined;
 
+    /** Refusal message. */
+    refusal?: MessageContent|undefined;
+
+    /** A list of tool calls. */
     tool_calls?: ToolCall[]|undefined;
+
+    /** Extra, provider-specific data. */
+    extra?: unknown;
 }
 
 export const Message: PublicType<Message> = exportType(type({
@@ -28,8 +45,11 @@ export const Message: PublicType<Message> = exportType(type({
 
     content: MessageContent,
     "reasoning?": MessageContent.or("undefined"),
+    "refusal?": MessageContent.or("undefined"),
 
     "tool_calls?": ToolCall.array().or("undefined"),
+
+    "extra?": "unknown",
 }));
 
 export const MessageArray = exportType(Message.array());
