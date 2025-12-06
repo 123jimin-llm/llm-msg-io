65abf7a887ff21e652c4836abd34a29db7d1ba52
Jimin Park <pjm950@gmail.com>
2025-12-07 01:22:45 +0900

    Removed null codec.

--- src/api-codec/null.ts
+++
@@ -1,6 +0,0 @@
-import type { Codec, MessageArrayLike } from "../message/index.ts";
-
-export const NullCodec = {
-    createEncoder: () => (messages) => messages,
-    createDecoder: () => (messages) => messages,
-} satisfies Codec<MessageArrayLike>;
