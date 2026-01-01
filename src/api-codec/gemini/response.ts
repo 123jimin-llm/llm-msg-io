4a15afdd4bb8fb074b4280d2e03d2e21a556f45b
Jimin Park <pjm950@gmail.com>
2026-01-01 14:39:02 +0900

    Writing GeminiGenerateContentResponseCodec...

--- src/api-codec/gemini/response.ts
+++ src/api-codec/gemini/response.ts
@@ -1,10 +1,31 @@
-import type { GenerateContentResponse } from "@google/genai";
+import type { FinishReason, GenerateContentResponse } from "@google/genai";
 
-import type { WithCreateStepDecoder } from "../../api-codec-lib/index.ts";
+import type { StepResult, WithCreateStepDecoder } from "../../api-codec-lib/index.ts";
+
+// Converts to OpenAI-compatible finish reason.
+export function fromGeminiFinishReason(finish_reason: FinishReason): string {
+    switch(finish_reason as string) {
+        case 'MAX_TOKENS': return 'length';
+        case 'SAFETY':
+        case 'RECITATION':
+        case 'BLOCKLIST':
+        case 'PROHIBITED_CONTENT':
+        case 'SPII': return 'content_filter';
+        case 'STOP':
+        case 'OTHER':
+        default: return 'stop';
+    }
+}
 
 export const GeminiGenerateContentResponseCodec = {
-    createStepDecoder: () => () => {
+    createStepDecoder: () => (api_res) => {
+        console.log(api_res);
+
         // TODO
-        throw new Error("Not yet implemented!");
+        const res: StepResult = {
+            messages: [],
+        };
+
+        return res;
     },
 } satisfies WithCreateStepDecoder<GenerateContentResponse>;
