4f745168d6d304755bf8c030d9dce328ced22bd8
Jimin Park <pjm950@gmail.com>
2026-01-02 00:15:40 +0900

    Implemented GeminiGenerateContentResponseCodec.

--- src/api-codec/gemini/response.ts
+++ src/api-codec/gemini/response.ts
@@ -1,6 +1,8 @@
-import type { FinishReason, GenerateContentResponse } from "@google/genai";
+import type { Content, FinishReason, GenerateContentResponse, Blob as GeminiBlob } from "@google/genai";
 
 import type { StepResult, WithCreateStepDecoder } from "../../api-codec-lib/index.ts";
+import { concatContentsTo, type ContentPart, type Message, type MessageContent } from "../../message/index.ts";
+import { getMessageExtraGemini } from "./extra.ts";
 
 // Converts to OpenAI-compatible finish reason.
 export function fromGeminiFinishReason(finish_reason: FinishReason): string {
@@ -17,14 +19,71 @@ export function fromGeminiFinishReason(finish_reason: FinishReason): string {
     }
 }
 
+export function fromGeminiBlob(blob: GeminiBlob): ContentPart|null {
+    if(blob.mimeType?.startsWith("image/")) {
+        return {
+            type: 'image',
+            url: `data:${blob.mimeType};base64,${blob.data}`,
+        };
+    }
+
+    return null;
+}
+
+export function fromGeminiContent(api_content: Content): Message {
+    const thought_signatures: string[] = [];
+
+    let content: MessageContent = "";
+    const reasonings: string[] = [];
+
+    for(const part of api_content.parts ?? []) {
+        if(part.thoughtSignature) {
+            thought_signatures.push(part.thoughtSignature);
+        }
+
+        if(part.inlineData) {
+            const blob_part = fromGeminiBlob(part.inlineData);
+            if(blob_part) {
+                content = concatContentsTo(content, [blob_part]);
+            }
+        }
+
+        if(!part.text) continue;
+        
+        if(part.thought) {
+            reasonings.push(part.text);
+        } else {
+            content = concatContentsTo(content, part.text);
+        }
+    }
+
+    const message: Message = {
+        role: 'assistant',
+        content,
+    };
+
+    if(reasonings.length) {
+        message.reasoning = reasonings.join('\n');
+    }
+
+    if(thought_signatures.length) {
+        const extra = getMessageExtraGemini(message, true);
+        extra.thought_signatures = thought_signatures;
+    }
+
+    return message;
+}
+
 export const GeminiGenerateContentResponseCodec = {
     createStepDecoder: () => (api_res) => {
-        console.log(api_res);
-
-        // TODO
         const res: StepResult = {
             messages: [],
         };
+
+        const candidate = api_res.candidates?.[0];
+        if(candidate?.content) {
+            res.messages.push(fromGeminiContent(candidate.content));
+        }
 
         return res;
     },
