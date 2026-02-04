51057e79e04c6890639a30863054ea0651b72009
Jimin Park <pjm950@gmail.com>
2026-02-04 23:35:39 +0900

    Removed API step wrapper (use briko instead).

--- src/api-codec-lib/step/index.ts
+++ src/api-codec-lib/step/index.ts
@@ -3,10 +3,9 @@ export * from "./response.ts";
 export * from "./stream.ts";
 export * from "./function.ts";
 
-import type { Nullable } from "../../util/type.ts";
-import { createStepEncoder, type StepParams, type WithCreateStepEncoder } from "./request.ts";
-import { createStepDecoder, type StepResult, type WithCreateStepDecoder } from "./response.ts";
-import { createStepStreamDecoder, type StepStream, type WithCreateStepStreamDecoder } from "./stream.ts";
+import type { StepParams, WithCreateStepEncoder } from "./request.ts";
+import type { StepResult, WithCreateStepDecoder } from "./response.ts";
+import type { WithCreateStepStreamDecoder } from "./stream.ts";
 
 export type APIStepCodec<
     APIRequestType, APIResponseType,
@@ -28,43 +27,3 @@ export type APIStepCodecWithStream<
 >
     = APIStepCodec<APIRequestType, APIResponseType, EncodeOptions, DecodeOptions, RequestType, ResponseType>
     & WithCreateStepStreamDecoder<APIStreamType, ResponseType, StreamDecodeOptions>;
-
-export type APIStep<
-    APIRequestBaseType, APIResponseType, APIStreamType,
-> = <IsStream extends boolean = false>(req: APIRequestBaseType & { stream?: Nullable<IsStream> }) => Promise<APIStreamType | APIResponseType>;
-
-export type StrictAPIStep<
-    APIRequestBaseType, APIResponseType, APIStreamType,
-> = <IsStream extends boolean = false>(req: APIRequestBaseType & { stream?: Nullable<IsStream> }) => Promise<IsStream extends true ? APIStreamType : APIResponseType>;
-
-export function wrapAPIStep<
-    APIRequestBaseType, APIResponseType, APIStreamType,
-    EncodeOptions extends object = object,
-    DecodeOptions extends object = object,
-    StreamDecodeOptions extends DecodeOptions = DecodeOptions,
-    RequestType extends StepParams = StepParams,
-    ResponseType extends StepResult = StepResult,
->(
-    codec: APIStepCodecWithStream<APIRequestBaseType & { stream?: Nullable<boolean> }, APIResponseType, APIStreamType, EncodeOptions, DecodeOptions, StreamDecodeOptions, RequestType, ResponseType>,
-    api: APIStep<APIRequestBaseType, APIResponseType, APIStreamType>,
-    encode_options?: EncodeOptions,
-    decode_options?: DecodeOptions,
-    stream_decode_options?: StreamDecodeOptions,
-): StrictAPIStep<RequestType, ResponseType, StepStream<ResponseType>> {
-    const stepEncoder = createStepEncoder<APIRequestBaseType & { stream?: Nullable<boolean> }, RequestType, EncodeOptions>(codec, encode_options);
-    const stepDecoder = createStepDecoder<APIResponseType, ResponseType, DecodeOptions>(codec, decode_options);
-    const stepStreamDecoder = createStepStreamDecoder<APIStreamType, ResponseType, StreamDecodeOptions>(codec, stream_decode_options);
-    
-    return async <IsStream extends boolean = false>(req: RequestType & { stream?: Nullable<IsStream> }): Promise<IsStream extends true ? StepStream<ResponseType> : ResponseType> => {
-        type ReturnType = IsStream extends true ? StepStream<ResponseType> : ResponseType;
-        const api_req = {...stepEncoder(req), stream: req.stream ?? false};
-        // Skip type-checking due to TS2589
-        const api_res = await (api as (api_req: unknown) => Promise<unknown>)(api_req);
-
-        if (req.stream) {
-            return stepStreamDecoder(api_res as APIStreamType) as ReturnType;
-        } else {
-            return stepDecoder(api_res as APIResponseType) as ReturnType;
-        }
-    };
-}
--- src/api-codec/openai/chat/index.ts
+++ src/api-codec/openai/chat/index.ts
@@ -3,26 +3,18 @@ export * from "./response.ts";
 export * from "./stream.ts";
 
 import type {
-    Completions,
     ChatCompletion,
     ChatCompletionCreateParamsBase,
-    ChatCompletionCreateParams,
 } from "openai/resources/chat/completions";
 
 import { OpenAIChatRequestCodec } from "./request.ts";
 import { OpenAIChatResponseCodec } from "./response.ts";
 import { OpenAIChatStreamCodec, type OpenAIChatCompletionStream } from "./stream.ts";
 
-import { wrapAPIStep, type StrictAPIStep, type APIStepCodecWithStream, type StepParams, type StepResult, type StepStream } from "../../../api-codec-lib/step/index.ts";
+import type { APIStepCodecWithStream, } from "../../../api-codec-lib/step/index.ts";
 
 export const OpenAIChatCodec = {
     ...OpenAIChatRequestCodec,
     ...OpenAIChatResponseCodec,
     ...OpenAIChatStreamCodec,
 } satisfies APIStepCodecWithStream<ChatCompletionCreateParamsBase, ChatCompletion, OpenAIChatCompletionStream>;
-
-export function wrapOpenAIChat(
-    api: (req: ChatCompletionCreateParams) => ReturnType<typeof Completions.prototype.create>,
-): StrictAPIStep<StepParams, StepResult, StepStream<StepResult>> {
-    return wrapAPIStep(OpenAIChatCodec, api);
-}
